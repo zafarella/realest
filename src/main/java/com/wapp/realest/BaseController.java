eec256bae79425c42e7f1282bcf6cda4f72f8f5a
zafarella Linux <zafarella@gmail.com>
2013-02-16 14:29:03 +0100

    --

--- src/main/java/com/wapp/realest/BaseController.java
+++ src/main/java/com/wapp/realest/BaseController.java
@@ -10,23 +10,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping("/")
 public class BaseController {
 
-	@RequestMapping(value = "/welcome", method = RequestMethod.GET)
-	public String welcome(ModelMap model) {
+    @RequestMapping(value = "/welcome", method = RequestMethod.GET)
+    public String welcome(ModelMap model) {
 
-		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - welcome()");
+        model.addAttribute("message", "Maven Web Project + Spring 3 MVC - welcome()");
 
-		// Spring uses InternalResourceViewResolver and return back index.jsp
-		return "index";
+        // Spring uses InternalResourceViewResolver and return back index.jsp
+        return "index";
 
-	}
+    }
 
-	@RequestMapping(value = "/welcome/{name}", method = RequestMethod.GET)
-	public String welcomeName(@PathVariable String name, ModelMap model) {
+    @RequestMapping(value = "/welcome/{name}", method = RequestMethod.GET)
+    public String welcomeName(@PathVariable String name, ModelMap model) {
 
-		model.addAttribute("message", "Maven Web Project + Spring 3 MVC - "
-				+ name);
-		return "index";
+        model.addAttribute("message", "Maven Web Project + Spring 3 MVC - "
+                + name);
+        return "index";
 
-	}
+    }
 
 }
